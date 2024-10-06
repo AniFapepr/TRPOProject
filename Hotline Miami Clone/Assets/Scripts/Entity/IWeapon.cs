bf9788e564e2a91ffc5b092f80cce92d0ecd8d87
AniFapper <viruns228555@gmail.com>
2024-10-06 15:47:46 +0300

    Test

---
+++ Hotline Miami Clone/Assets/Scripts/Entity/IWeapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts.Entity{
+   public interface IWeapon
+   {
+    public bool use();
+    
+   }  
+
+}
--- Hotline Miami Clone/Assets/Scripts/Entity/Player/CameraScript.cs
+++ Hotline Miami Clone/Assets/Scripts/Entity/Player/CameraScript.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     // Start is called before the first frame update
-   Transform player;
+    Transform player;
     bool followPlayer = true;
     // Use this for initialization
     void Start () 
     {
 
         player = FindObjectOfType<CharacterMovement>().transform;
-        if (player == null) {
+        if (player == null) 
+        {
         Debug.LogError("Персонаж не найден!");
         }
 
     }
     // Update is called once per frame
-    void Update () {
-        if (followPlayer == true) {
+    void Update () 
+    {
+        if (followPlayer == true) 
+        {
             camFollowPlayer();
         }
     }
    
+   public void setFollowPlayerStatus(bool var)
+   {
+    
+    this.followPlayer = var;
+
+   }
 
     void camFollowPlayer()
     {
